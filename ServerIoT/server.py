bf60d0996d87cfccd39c928f417a844b4fca8aa7
Kyala16 <artem.myakishev.2018@mail.ru>
2023-07-12 14:10:09 +0300

    test

--- ServerIoT/server.py
+++ ServerIoT/server.py
@@ -1,4 +1,5 @@
 from paho.mqtt import client as mqtt_client
+import ssl
 
 #Brocker info
 broker = '10.0.41.165'	# broker ip
@@ -15,17 +16,14 @@ client_id = f'publish-{999}' # generate this
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe(my_topics)
-    print(0)
 
 
 def client_forever():
-
 	client = mqtt_client.Client()
 	client.on_connect = on_connect
-
+	client.tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLS, ciphers=None)
 	client.connect(broker, port, 60)
 	client.loop_forever()
-
 
 
 if __name__ == "__main__":  
